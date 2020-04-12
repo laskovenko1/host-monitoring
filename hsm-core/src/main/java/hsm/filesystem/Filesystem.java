285b9342b467a55895c36d3380a8ab68c6d517a8
laskovenko1 <laskovenko1@yandex.ru>
2020-04-12 17:43:49 +0300

    Filesystem API added

---
+++ hsm-core/src/main/java/hsm/filesystem/Filesystem.java
@@ -0,0 +1,46 @@
+package hsm.filesystem;
+
+import hsm.units.InformationQuantity;
+
+public class Filesystem {
+
+    private final String name;
+    private final String type;
+    private final InformationQuantity size;
+    private final InformationQuantity used;
+    private final InformationQuantity available;
+    private final String mountPoint;
+
+    public Filesystem(String name, String type, InformationQuantity size, InformationQuantity used, InformationQuantity available, String mountPoint) {
+        this.name = name;
+        this.type = type;
+        this.size = size;
+        this.used = used;
+        this.available = available;
+        this.mountPoint = mountPoint;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public InformationQuantity getSize() {
+        return size;
+    }
+
+    public InformationQuantity getUsed() {
+        return used;
+    }
+
+    public InformationQuantity getAvailable() {
+        return available;
+    }
+
+    public String getMountPoint() {
+        return mountPoint;
+    }
+}
